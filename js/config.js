fa9e46de2d647642fe95f3174c76f1d817b98f9a
Kanat3108 <25495756+Kanat3108@users.noreply.github.com>
2025-01-29 19:50:41 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_J21ifeG50', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3okfl9gg9ogs8pbt137pkda3it', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_T5F34eDur', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4bneuoplav72l9j3nnj17ed472', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
